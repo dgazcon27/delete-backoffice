fb4b13c4511ee9734098714826f9944ee4ac9d5d
Gregory <gregory.facyt@gmail.com>
2018-06-26 10:43:49 -0400

    + Se reorganizo los estilos y la imagen del logo en carpetas dentro del component Login

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -5,79 +5,14 @@ import { compose } from 'redux';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import logo from './logo-delete.svg';
+import styles from './css/loginCss';
+import logo from './image/logo-delete.svg';
 
 import {
 	setEmail,
 	setPassword,
 	requestLogin,
 } from '../../actions/Login/actionsCreators';
-
-const styles = theme => ({
-	root: {
-		flexGrow: 1,
-	},
-	paper: {
-		padding: theme.spacing.unit * 2,
-		textAlign: 'center',
-		color: theme.palette.text.secondary,
-	},
-	button: {
-		margin: theme.spacing.unit,
-	},
-	login: {
-		'border-radius': '0.3em',
-		'-webkit-box-shadow': '1px 3px 8px 3px #8e8e8e73',
-		'box-shadow': '1px 3px 8px 3px #8e8e8e73',
-		border: '3px solid #455A64',
-		padding: '54px',
-		'text-align': 'left',
-		'background-color': '#ffffff',
-	},
-	centerLogin: {
-		display: 'flex',
-		'justify-content': 'center',
-		'align-items': 'center',
-		height: '60vh',
-	},
-	enterButton: {
-		float: 'right',
-		'background-color': '#455A64 !important',
-	},
-	recoverPassword: {
-		float: 'left',
-		'margin-top': '0.7vh',
-		color: '#90A4AE',
-	},
-	marginButtons: {
-		'margin-top': '8vh',
-	},
-	centerLogo: {
-		display: 'flex',
-		'justify-content': 'center',
-		'align-items': 'center',
-		'margin-top': '15vh',
-	},
-	label: {
-		'font-size': '14px',
-	},
-	inputs: {
-		'border-bottom': '2px solid #455A64 !important',
-		'font-size': '16px !important',
-	},
-	error: {
-		'background-color': '#455A64',
-		color: 'white',
-		height: '9%',
-		'margin-top': '22.5vh',
-		padding: '31px',
-		position: 'absolute',
-		width: 'auto',
-		'-webkit-box-shadow': '1px 3px 8px 3px #8e8e8e73',
-		'box-shadow': '1px 3px 8px 3px #8e8e8e73',
-		'font-size': '16px',
-	},
-});
 
 const Login = ({
 	error,
