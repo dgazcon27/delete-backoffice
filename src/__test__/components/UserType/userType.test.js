fad1d08fc461a7b7fe9c6bd4084c130c89049f50
Gregory <gregory.facyt@gmail.com>
2018-07-17 13:18:53 -0400

    Se logro renderizar component userType pasandole informacion de la query

--- src/__test__/components/UserType/userType.test.js
+++ src/__test__/components/UserType/userType.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import Adapter from 'enzyme-adapter-react-16';
 import { configure } from 'enzyme';
 import { MockedProvider } from 'react-apollo/test-utils';
-// import { GET_ROLES } from '../../../queries/userType';
+import { GET_ROLES } from '../../../queries/userType';
 import { UserTypeTest } from '../../../components/UserType/userType';
 // import wait from 'waait';
 
@@ -30,4 +30,64 @@ it('should render without error', () => {
 	};
 
 	renderer.create(<MockedProvider mocks={[]}><UserTypeTest {...props} /></MockedProvider>);
+});
+
+
+it('should render loading state initially', () => {
+	const props = {
+		id: 1,
+		name: '',
+		isOpen: false,
+		classes: {},
+		modalType: '',
+		statusValue: 0,
+		paginationPage: 1,
+		currentPage: 0,
+		actionSetRol: jest.fn(),
+		actionOpenModal: jest.fn(),
+		actionCloseModal: jest.fn(),
+		blockRolMutation: jest.fn(),
+		deleteRolMutation: jest.fn(),
+		actionBlockUserType: jest.fn(),
+		actionDeleteUserType: jest.fn(),
+		actionChangePage: jest.fn(),
+	};
+
+	const paginationPage = 1;
+	const roles = {
+		data: [{
+			name: 'ADMIN',
+			id: 1,
+			description: 'ADMIN',
+			status: {
+				id: 1,
+			},
+		},
+		{
+			name: 'OWNER',
+			id: 2,
+			description: 'OWNER',
+			status: {
+				id: 1,
+			},
+		},
+		{
+			name: 'SOPORTE',
+			id: 3,
+			description: 'SOPORTE',
+			status: {
+				id: 1,
+			},
+		}],
+	};
+
+	const mocks = [{
+		request: {
+			query: GET_ROLES,
+			variables: { paginationPage },
+		},
+		result: { data: { roles } },
+	}];
+
+	renderer.create(<MockedProvider mocks={mocks}><UserTypeTest {...props} /></MockedProvider>);
 });
