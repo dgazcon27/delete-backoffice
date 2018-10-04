40bba0debfd003e21203d2eb20009435654141aa
Gregory <gregory.facyt@gmail.com>
2018-10-04 11:25:59 -0400

    se guardo que el ultimo testFinal sea el de location en el main

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -21,7 +21,7 @@ import PayCreate from '../PurchaseRequest/PayCreate';
 import Payment from '../Payment/payment';
 import Access from '../Access/access';
 import AccessCreate from '../Access/accessCreate';
-import TestFinal from '../UserType/testFinal';
+import TestFinal from '../Location/testFinal';
 import EditionComponent from '../Shared/editionComponent';
 
 const Main = (props) => {
