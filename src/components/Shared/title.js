a1532e40d24f9d6cf6a5ec37014c1a02d94828f3
Cjescalona1 <cjescalona1@gmail.com>
2018-11-27 22:23:55 -0400

    static title component 2

---
+++ src/components/Shared/title.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'react-apollo';
+import { withStyles } from '@material-ui/core/styles';
+import BackButton from '../widget/BackButton';
+import styles from './sharedStyles';
+// import { connect } from 'react-redux';
+
+const Title = ({ classes, title }) =>
+	(
+		<div>
+			<h4 className={classes.left}> {title} </h4>
+			<div className={classes.right}>
+				<BackButton />
+			</div>
+		</div>
+	);
+
+Title.propTypes = {
+	classes: PropTypes.object.isRequired,
+	title: PropTypes.string.isRequired,
+};
+export default compose(withStyles(styles, { withTheme: true }))(Title);
